1d1a08bbf9f22ada5eebc65daa0804ae5989a86e
linlongye <linlongye@126.com>
2018-12-01 20:22:07 +0800

    add for add switch

--- src/for/main.go
+++ src/for/main.go
@@ -83,4 +83,13 @@ outTer:
 			fmt.Println()
 		}
 	}
+
+	//九九乘法表
+	var num2 = 20
+	for i := 1; i <= num2; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %d\t", j, i, i*j)
+		}
+		fmt.Println()
+	}
 }
