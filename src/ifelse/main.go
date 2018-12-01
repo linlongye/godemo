9ab0df466f879d2f04b00782b2c365d0e9a47ccb
linlongye <linlongye@126.com>
2018-12-01 19:26:35 +0800

    add ifelse

--- src/ifelse/main.go
+++ src/ifelse/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var age int
+	fmt.Println("请输入年龄:")
+	_, err := fmt.Scanln(&age)
+	if err != nil {
+		println(err)
+	} else {
+		if age > 18 {
+			fmt.Println("你的年龄是", age)
+		}
+	}
 }
